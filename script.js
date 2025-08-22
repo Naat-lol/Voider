0d5cac304e9e8f6853bbf69143fe1e987b3f1db8
Naat-lol <nativeluiz100@gmail.com>
2025-08-22 14:16:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// app.js
-
-const fillBtn = document.getElementById('fillBtn');
-
-// Botão não faz nada, só dá um feedback visual no console
-fillBtn.addEventListener('click', () => {
-  console.log("[The Voider] fill the fog clicado (inativo).");
+// Só pra efeito, quando clicar no botão
+document.querySelector('.btn').addEventListener('click', () => {
+    alert('The fog is filling...');
 });
